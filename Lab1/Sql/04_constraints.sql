8bba7a5d2a302ec40368c24fcf3225f6a236ad0e
Nazim_fnz78 <farhannazimfnz@gmail.com>
2026-04-18 08:06:21 +0530

    Update 04_constraints.sql

--- Lab1/Sql/04_constraints.sql
+++ Lab1/Sql/04_constraints.sql
@@ -9,3 +9,31 @@ CHECK(member_id>=1000 AND member_id<=9999);
 ALTER TABLE Booktype 
 ADD CONSTRAINT chk_type_id 
 CHECK(type_id>=1000 AND type_id<=9999);
+
+
+
+
+
+
+
+
+
+-- Book CHECK constraint
+
+INSERT INTO Book VALUES (999,'AI','jane doe',1002);
+
+ERROR 3819 (HY000): Check constraint 'chk_book_id' is violated.
+
+
+-- Membership CHECK constraint
+
+INSERT INTO Membership VALUES (12,4);
+
+ERROR 3819 (HY000): Check constraint 'chk_member_id' is violated.
+
+
+-- Booktype CHECK constraint
+
+INSERT INTO Booktype VALUES (899,'history');
+
+ERROR 3819 (HY000): Check constraint 'chk_type_id' is violated.
